74fae638f1325859f1da905cd34ece1df969aa88
tuUsuarioEnGithub <tuCorreoEnGithub>
2024-08-30 07:25:51 -0600

    desc

--- routes/rutas.js
+++ routes/rutas.js
@@ -4,11 +4,11 @@ var hora=require("../pruebas/middles").hora;
 var bienvenida=require("../pruebas/middles").bienvenida;
 
 // Define tus rutas
-router.get('/',bienvenida, (req, res) => {
+router.get('/',bienvenida,hora ,(req, res) => {
     res.send('Hello es raiz');
 });
 
-router.get('/venta',hora,(req, res) => {
+router.get('/venta',hora,bienvenida,(req, res) => {
     res.send('Hi es venta');
 });
 
